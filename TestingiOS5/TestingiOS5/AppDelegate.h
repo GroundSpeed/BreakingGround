5a71682a8137669b93625966dffac665d9b65c46
Don Miller <don@groundspeedhq.com>
2016-02-01 14:52:36 -0500

    Added testing for unit testing talk

---
+++ TestingiOS5/TestingiOS5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingiOS5
+//
+//  Created by Don Miller on 1/10/16.
+//  Copyright © 2016 GroundSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingiOS5/TestingiOS5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingiOS5
+//
+//  Created by Don Miller on 1/10/16.
+//  Copyright © 2016 GroundSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
